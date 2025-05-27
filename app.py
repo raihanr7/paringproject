09704135c44055606a66553e9d8d8f1b5401c616
raihanr7 <raihannesatta@gmail.com>
2025-05-27 15:10:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-conn = psycopg2.connect("postgresql://postgres:***REMOVED***@db.wtmfsznnmyinbgzkdofz.supabase.co:5432/postgres")
+conn = psycopg2.connect(
+    host="db.wtmfsznnmyinbgzkdofz.supabase.co",
+    database="postgres",
+    user="postgres",
+    password="***REMOVED***",
+    port="5432"
+)
 
 FIELD_ORDER = {
     "Palapa_Ring_Barat_Alur": [
