2c11dbaac98746cf310c26bb49a424dc4d1cd7dc
raihanr7 <raihannesatta@gmail.com>
2026-01-09 10:48:52 +0700

    add python dotenv module

--- run_dev.ps1
+++ run_dev.ps1
@@ -6,7 +6,7 @@ Write-Host "Setting environment variables..." -ForegroundColor Green
 $env:DATABASE_HOST = "aws-0-ap-southeast-1.pooler.supabase.com"
 $env:DB_NAME = "postgres"
 $env:DB_USER = "postgres.wtmfsznnmyinbgzkdofz"
-$env:DB_PASSWORD = "palaparingproject123"
+$env:DB_PASSWORD = "palaparingproject12345"
 $env:DB_PORT = "6543"
 
 Write-Host "Environment variables set!" -ForegroundColor Green
